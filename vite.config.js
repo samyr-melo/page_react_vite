d25f9990d0cc0d565b8f94dd8c6d6cb9d9393bf0
Samyr Melo <samyrdiamantino591@gmail.com>
2025-03-26 17:39:35 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/page_react_vite"
+  base: "/page_react_vite",
   
 })
